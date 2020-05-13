3b01ccc5a3e69c84485ac3f6cfd3039e22ed3f1f
thibault mambour <thibault.mambour@protonmail.com>
2020-05-13 13:02:42 +0200

    Exericse 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+brouette = Restaurant.new(name: "La Brouette", category: "french", address: "75010 Paris")
+brouette.save
+yummy = Review.new(content: "yummy yummy", rating: 4)
+yummy.restaurant = brouette
+yummy.save
+mi_yummy = Review.new(content: "yummy", rating: 3)
+mi_yummy.restaurant = brouette
+mi_yummy.save
+
+argent = Restaurant.new(name: "La Tour d'Argent", category: "french", address: "75015 Paris")
+argent.save
+
+bastoche = Restaurant.new(name: "La Bastoche", category: "belgian", address: "1050 Bruxelles")
+bastoche.save
+
+ramen = Restaurant.new(name: "Ramen", category: "japanese", address: "1000 Bruxelles")
+ramen.save
+
+bol = Restaurant.new(name: "Bon bol", category: "chinese", address: "1000 Bruxelles")
+bol.save
